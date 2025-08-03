ac2714d0586bad7c37fe962680dbd234ccf453e2
KasimSache <45384577+MdKasimS@users.noreply.github.com>
2025-08-03 22:45:41 +0530

    resovled references after re-structring

--- Services/ResourceMonitorService.cs
+++ Services/ResourceMonitorService.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using SystemMonitoringTool.Classes.Config;
+using SystemMonitoringTool.Classes.Plugins;
 using SystemMonitoringTool.Models;
 
 namespace SystemMonitoringTool.Services
